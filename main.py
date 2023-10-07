2b243517921f6a9a76926cf4944566dc205e1771
MauricioLopezJara <94513308+MauricioLopezJara@users.noreply.github.com>
2023-10-07 17:19:06 -0600

    main.py

--- main.py
+++ main.py
@@ -11,17 +11,16 @@ st.set_page_config(
 def pagina_bienvenida():
     # Crea 3 columnas
     left_col, mid_col, right_col = st.columns(3)
-
     # Muestra la imagen en la columna central
     mid_col.image("Nasa.png", width=200)
     st.title("Bienvenido a la página de la NASA sobre la Reconexión Magnética de la Tierra")
     st.write("La reconexión magnética es un proceso importante en la interacción entre el viento solar y el campo magnético de la Tierra.")
     st.header("Cómo afecta la reconeccion magnetica")
     st.write("La reconexión magnética puede tener un impacto en la calidad de servicios generados por medio de campos electromagneticos")
-    st.write("⊛ La calidad de Servicios")
-    st.write("⊛ Sistemas de navegacion")
-    st.write("⊛ Las comunicaciones")
-    st.write("⊛ Daña sistemas electronicas de naves espaciales")
+    st.write("☆ La calidad de Servicios")
+    st.write("☆ Sistemas de navegacion")
+    st.write("☆ Las comunicaciones")
+    st.write("☆ Daña sistemas electronicas de naves espaciales")
 
 # Función para la página de información (Opción 2)
 def Datos():
@@ -31,7 +30,17 @@ def Datos():
 # Función para la página de contacto (Opción 3)
 def pagina_contacto():
     st.title("Contáctenos")
+    st.title("Contáctenos")
+    st.write("Si tienes preguntas o comentarios, no dudes en ponerte en contacto con nosotros.")
+  
+  # Formulario de contacto
+  with st.form("formulario_contacto"):
+    nombre = st.text_input("Nombre", required=True)
+    correo = st.text_input("Correo Electrónico", required=True)
+    mensaje = st.text_area("Mensaje", required=True)
+    enviar = st.form_submit_button("Enviar")
     # Agrega información de contacto si lo deseas
+
 
 # Título del menú
 st.sidebar.markdown("Bienvenido al Menu")
