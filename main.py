05daddbd9fae497ab410dbd2d67722edf341004e
MauricioLopezJara <94513308+MauricioLopezJara@users.noreply.github.com>
2023-10-07 17:41:52 -0600

    main.py

--- main.py
+++ main.py
@@ -8,28 +8,20 @@ st.set_page_config(
 )
 
 # Función para la página de bienvenida
-def pagina_contacto():
-  st.title("Contáctenos")
-  st.write("Si tienes preguntas o comentarios, no dudes en ponerte en contacto con nosotros.")
-  
-  # Formulario de contacto
-  with st.form("formulario_contacto"):
-    nombre = st.text_input("Nombre")
-    correo = st.text_input("Correo Electrónico")
-    mensaje = st.text_area("Mensaje")
-    enviar = st.form_submit_button("Enviar")
-    
-  if enviar:
-    # Validación de campos obligatorios
-    if not nombre or not correo or not mensaje:
-      st.warning("Por favor, completa todos los campos obligatorios.")
-    else:
-      # Procesamiento del formulario aquí
-      # Por ejemplo, puedes almacenar los datos en una base de datos
-      st.success(f"Gracias por contactarnos, {nombre}! Hemos recibido tu mensaje.")
-      # Agrega un botón para volver a la página de bienvenida
-      st.button("Volver a la página de bienvenida", on_click=pagina_bienvenida)
+# Función para la página de bienvenida
+def pagina_bienvenida():
+    # Crea 3 columnas
+    left_col, mid_col, right_col = st.columns(3)
 
+    # Muestra la imagen en la columna central
+    mid_col.image("Nasa.png", width=200)
+    st.title("Bienvenido a la página de la NASA sobre la Reconexión Magnética de la Tierra")
+    st.write("La reconexión magnética es un proceso importante en la interacción entre el viento solar y el campo magnético de la Tierra.")
+    st.header("Cómo afecta la reconeccion magnetica")
+    st.write("La reconexión magnética puede tener un impacto en la calidad de servicios generados por medio de campos electromagneticos")
+    # Botón
+    if st.button("¡Celebremos con globos!"):
+        st.balloons()
 
 # Función para la página de información (Opción 2)
 def Datos():
