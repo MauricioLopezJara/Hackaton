0896a1c2574f2ac038a58c81a69609b27d0fd65e
MauricioLopezJara <94513308+MauricioLopezJara@users.noreply.github.com>
2023-10-07 18:25:32 -0600

    main.py

--- main.py
+++ main.py
@@ -39,17 +39,17 @@ def pagina_contacto():
     # Crea 3 columnas
 
 
-Uno_col, Dos_col, Tres_col, Cuatro_col, Cinco_col, Seis_col = st.columns(6)
+    Uno_col, Dos_col, Tres_col, Cuatro_col, Cinco_col, Seis_col = st.columns(6)
 
-# Muestra la imagen en la columna central
-Uno_col.image("Yael.jpeg", width=200)
-Dos_col.image("Mauricio.jpeg", width=200)
-Tres_col.image("Brian.jpeg", width=200)
-Cuatro_col.image("Dani.jpeg", width=200)
-Cinco_col.image("Ange.jpeg", width=200)
-Seis_col.image("Franc.jpeg", width=200)
+    # Muestra la imagen en la columna central
+    Uno_col.image("Yael.jpeg", width=200)
+    Dos_col.image("Mauricio.jpeg", width=200)
+    Tres_col.image("Brian.jpeg", width=200)
+    Cuatro_col.image("Dani.jpeg", width=200)
+    Cinco_col.image("Ange.jpeg", width=200)
+    Seis_col.image("Franc.jpeg", width=200)
 
-st.button("Volver a la página de bienvenida", on_click=pagina_bienvenida)
+    st.button("Volver a la página de bienvenida", on_click=pagina_bienvenida)
 
 # Título del menú
 st.sidebar.markdown("Bienvenido al Menu")
