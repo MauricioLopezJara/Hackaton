321436688cfbc3509101f107e27f5b3b28fb6123
MauricioLopezJara <94513308+MauricioLopezJara@users.noreply.github.com>
2023-10-08 08:07:23 -0600

    Analisis de senales.py

--- Analisis de senales.py
+++ Analisis de senales.py
@@ -2,26 +2,18 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import scipy as sp
 
-headers = ['year', 'day', 'hour', 'B_scalar','Bz_GSM','Bz_GSE','SW_temperature', 'SW_proton_density','Plasma_Speed','Flow_pressure']
-df = pd.read_csv('data1.txt', delim_whitespace=' ',names=headers)
-#Found max values for each column
-df.max()
-#Create list with max values of each column
-max_values = [9999, 999, 99, 99, 99, 99, 9999999.00, 999.90, 9999.00, 99.99]
-# Remove rows with missing values
-df_raw = df[(df != max_values).all(axis=1)]
+url="https://danielperez.pythonanywhere.com/get_past_peeks/"
+resp = requests.get(url)
 
-# reset index
-df_raw = df_raw.reset_index(drop=True)
+df = pd.DataFrame(resp.json()["DataFrame"].values(),index=resp.json()["DataFrame"].keys())
 
-data_to_find = df_raw['Bz_GSM']
+time = np.array(resp.json()['time'])
+lista = np.array(df)
+value = np.array(lista[time])
 
-peaks = sp.signal.find_peaks(data_to_find, height=3.88, distance=20)
-print(len(peaks[0]))
-#plot Bz and peaks
-plt.plot(data_to_find)
-plt.plot(peaks[0], data_to_find[peaks[0]], "o")
+plt.plot(lista)
+plt.plot(time, value[time], "o")
 plt.show()
-data_to_find[peaks[0]].shape
-df_raw.describe()
-data_to_find[peaks[0]].mean()
+value[time].shape
+#df_raw.describe()
+value[time].mean()
