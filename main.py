637bddf2fef7e8915e75d4345487f4d05564dbba
MauricioLopezJara <94513308+MauricioLopezJara@users.noreply.github.com>
2023-10-08 11:53:24 -0600

    Update main.py

--- main.py
+++ main.py
@@ -83,7 +83,7 @@ def predict():
     url="https://DanielPerez.pythonanywhere.com/predictions/"
     resp = requests.get(url)
     
-    df = pd.DataFrame([resp.json()["fechas"],resp.json()["pronostico"]],columns=['fechas','pronostico'])
+    df = pd.DataFrame({'fechaas':resp.json()["fechas"],'pronostico':resp.json()["pronostico"]})
     
     # Crear una figura para la gráfica
     fig, ax = plt.subplots()
