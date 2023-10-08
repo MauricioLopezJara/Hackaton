7e09b27432d2ee91a3825a315f1e999c37b3a0aa
MauricioLopezJara <94513308+MauricioLopezJara@users.noreply.github.com>
2023-10-07 20:26:57 -0600

    main.py

--- main.py
+++ main.py
@@ -16,11 +16,9 @@ def pagina_bienvenida():
     # Muestra la imagen en la columna central
     mid_col.image("Nasa.png", width=200)
     st.title("Bienvenido a la página de la NASA sobre la Reconexión Magnética de la Tierra")
-    st.write(
-        "La reconexión magnética es un proceso importante en la interacción entre el viento solar y el campo magnético de la Tierra.")
+    st.write("La reconexión magnética es un proceso importante en la interacción entre el viento solar y el campo magnético de la Tierra.")
     st.header("Cómo afecta la reconeccion magnetica")
-    st.write(
-        "La reconexión magnética puede tener un impacto en la calidad de servicios generados por medio de campos electromagneticos")
+    st.write("La reconexión magnética puede tener un impacto en la calidad de servicios generados por medio de campos electromagneticos")
     # Botón
     if st.button("¡Celebremos con globos!"):
         st.balloons()
@@ -37,10 +35,7 @@ def pagina_contacto():
     st.title("Contáctenos")
     st.write("Si tienes preguntas o comentarios, no dudes en ponerte en contacto con nosotros.")
     # Crea 3 columnas
-
-
     Uno_col, Dos_col, Tres_col, Cuatro_col, Cinco_col, Seis_col = st.columns(6)
-
     # Muestra la imagen en la columna central
     Uno_col.image("1.png", width=110)
     Dos_col.image("2.png", width=110)
